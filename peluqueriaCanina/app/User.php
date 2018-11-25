b816ce4c52846d219ac9bbf5442e59f2e78e5fd1
PedroIgnacioMM <pedro.ignaciom95@gmail.com>
2018-11-25 04:21:43 -0300

    feat: Se agrega la imagen a los datos fillables

--- peluqueriaCanina/app/User.php
+++ peluqueriaCanina/app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','nickname','rut','telefono','ciudad','direccion','edad','sexo',
+        'name', 'email', 'password','nickname','rut','telefono','ciudad','direccion','edad','sexo','imagen',
     ];
     protected $primaryKey = 'id';
 
