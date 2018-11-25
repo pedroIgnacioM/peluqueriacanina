05a2483b746af4b6a0b61d6a89465e87ee61e92f
karina bernal <karina.bernalb2015@gmail.com>
2018-11-25 04:23:59 -0300

    perfil usuario mostrar datos (usuario y mascotas)

--- peluqueriaCanina/resources/views/perfilUsuario.blade.php
+++ peluqueriaCanina/resources/views/perfilUsuario.blade.php
@@ -2,24 +2,71 @@
 
 @section('content')
 <div class="container">
-        <div class="justify-content-center">
                 <div class="card">
                     <div class="card-body">
+
                             <div class="card-title"><h1>Perfil Usuario</h1></div>
+                            
+                                            <div class="row ">
+                                                <div class="col-md-4">
+                                                        <div class="card">
+                                                                <div class="card-body">                            
+                                                                    <div class="card-title"><h1>Imangen Perfil Usuario</h1></div>
 
-        <table class="table table-hover">
-            {{ csrf_field() }}
-                @foreach (array_combine($titulos, $nombresColumnas) as $titulo => $nombreColumna)
-                        <a class="text">{{$titulo}}:{{$usuario->$nombreColumna}}</a><br>
-                
-                @endforeach
+                                               
+                                                                    </div>
+                                                                </div>
+                                                        </div>
+                                                <div class="col-md-8">
+                                                        <div class="card">
+                                                                <div class="card-body">                            
+                                                                    <div class="card-title"><h1>Datos Usuario</h1></div>
 
-       
-            </div>
+                                                                  
+                                                                    {{ csrf_field() }}
+                                                                    @foreach (array_combine($titulos, $nombresColumnas) as $titulo => $nombreColumna)
+                                                                            <b >{{$titulo}}</b>:<a class="text">{{$usuario->$nombreColumna}}</a><br>
+                                                                    
+                                                                    @endforeach
+                                                                    </div>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                        
+                                            </div>
+                                            <div class="row ">
+                                                    <div class="col-md-12">
+                                                            <div class="card">
+                                                                    <div class="card-body">                            
+                                                                        <div class="card-title"><h1>Mascotas</h1></div>
+    
+                                                                        <table class="table">
+                                                                                {{ csrf_field() }}
+                                                                            <tr>
+                                                                              <th>Nombre</th>
+                                                                              <th>Sexo</th>
+                                                                              <th>Edad</th>
+                                                                              <th>Color</th>
+                                                                            </tr>
+                                                                            @foreach ($mascotas as $mascota)
+                                                                            <tr>
+                                                                            @foreach ($nombresColumnasMascotas as $nombreColumnaMascota)
+                                                        
+                                                                            <td>{{$mascota->$nombreColumnaMascota}}</td>
+                                                                            @endforeach
+                                                                            <td><a class="text" href="#">Ver Ficha</a></td>
+                                                                             
+                                                                            @endforeach
+                                                                            </tr>
+                                                                        </table>
+                                                                        </div>
+                                                                    </div>
+                                                            </div>
+                                                        
+                                                    </div>
+                                            </div>
 
-            </div>
-
-        </div>
-    </div>
-
+                        </div>
+                 </div>
+</div>
 @endsection
