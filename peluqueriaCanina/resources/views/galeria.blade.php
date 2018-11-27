7307c219a5098dcc12480e7319850f1739e74cb5
Valentina <valentina.reinoso.r@mail.pucv.cl>
2018-11-27 01:02:07 -0300

    Galeria
    galeria compilada sin el filtro implementado

--- peluqueriaCanina/resources/views/galeria.blade.php
+++ peluqueriaCanina/resources/views/galeria.blade.php
@@ -32,19 +32,19 @@
                                                     <div class="col-sm-8">
                                                         <div class="row">
                                                             <div class="form-check">
-                                                                <input class="form-check-input" type="checkbox" value="grande" id="chechgrande">
-                                                                <label class="form-check-label" for="defaultCheck1">Grande'</label>
+                                                                <input class="form-check-input" type="checkbox" value="grande" id="defaultCheck1">
+                                                                <label class="form-check-label" for="defaultCheck1">Grande</label>
                                                             </div>
                                                         </div>
                                                          <div class="row">
                                                             <div class="form-check">
-                                                                <input class="form-check-input" type="checkbox" value="mediano" id="checkMediano">
+                                                                <input class="form-check-input" type="checkbox" value="mediano" id="defaultCheck1">
                                                                 <label class="form-check-label" for="defaultCheck1">Mediano</label>
                                                             </div>
                                                         </div>
                                                          <div class="row">
                                                             <div class="form-check">
-                                                                <input class="form-check-input" type="checkbox" value="pequeño" id="checkPequeño">
+                                                                <input class="form-check-input" type="checkbox" value="pequeño" id="defaultCheck1">
                                                                 <label class="form-check-label" for="defaultCheck1">Pequeño</label>
                                                             </div>
                                                         </div>
@@ -57,18 +57,19 @@
                                                     <div class="col-sm-8">
                                                          <div class="row  ">
                                                             <div class="form-check">
-                                                                <input class="form-check-input" type="checkbox" value="rubio" id="checkRubio">
+                                                                <input class="form-check-input" type="checkbox" value="rubio" id="defaultCheck1">
                                                                 <label class="form-check-label" for="defaultCheck1">Rubio</label>
                                                             </div>
                                                         </div>
+                                                         <div class="row ">
                                                             <div class="form-check">
-                                                                <input class="form-check-input" type="checkbox" value="castaño" id="checkCastaño">
+                                                                <input class="form-check-input" type="checkbox" value="castaño" id="defaultCheck1">
                                                                 <label class="form-check-label" for="defaultCheck1">Castaño</label>
                                                             </div>
                                                         </div>
                                                            <div class="row">
                                                             <div class="form-check">
-                                                                <input class="form-check-input" type="checkbox" value="pelo_liso" id="checkPeloLiso">
+                                                                <input class="form-check-input" type="checkbox" value="pelo_liso" id="defaultCheck1">
                                                                 <label class="form-check-label" for="defaultCheck1">Pelo Liso</label>
                                                             </div>
                                                         </div>
@@ -77,7 +78,7 @@
                                                 <br>
                                                 <div class="row justify-content-center">  
                                                     <div class="col-sm-8">
-                                                        <button type="submit" id='buscar'class="btn btn-primary">{{ __('Buscar') }}</button>
+                                                        <button type="submit" class="btn btn-primary">{{ __('Buscar') }}</button>
                                                     </div>
                                                 </div>
                                             </form>
@@ -85,64 +86,15 @@
                                     </div>
                                 </div>
                             </div>
+                            
                         </div>
 
                         <div class="col-sm-10">
                             <div class='row galeria'>
-                                <?php
-                                if(!isset($_POST["buscar"])){
-
-                                    if (!isset($_POST['checkgrande'])) {
-                                        $arrays[] = $_POST['checkgrande'];
-                                    }
-                                    if (!isset($_POST["checkMediano"])) {
-                                        $arrays[] = $_POST["checkMediano"];
-                                    }
-                                    if (!isset($_POST["checkPequeño"])) {
-                                        $arrays[] = $_POST["checkPequeño"];
-                                    }
-                                    if (!isset($_POST["checkRubio"])) {
-                                        $arrays[] = $_POST["checkRubio"];
-                                    }
-                                    if (!isset($_POST["checkCastaño"])) {
-                                        $arrays[] = $_POST["checkCastaño"];
-                                    }
-                                    if (!isset($_POST["checkPeloLiso"])) {
-                                        $arrays[] = $_POST["checkPeloLiso"];
-                                    }
-
-                                    if($cortePelos->count()){
-                                        foreach($cortePelos as $cortePelo){
-                                            foreach($arrays as $array){
-                                                if($array == $cortePelo->tamaño || $array == $cortePelo->tipoCabello){ ?>
-                                                    <div class="col-sm-4 ">
-                                                        <div class="img-container">
-                                                            <div class="panel-body justify-content-center imagen">
-                                                                <a class="thumbnail fancybox" rel="ligthbox" href="/cortePelo/{{ $cortePelo->imagen}}">
-                                                                    <img class="img-responsive" alt="" src="/cortePelo/{{ $cortePelo->imagen }}" width="300" />
-                                                                    <p>{{$cortePelo->descripcion}}</p>
-                                                                </a> 
-                                                            </div>
-                                                        </div>
-                                                        <div class="panel-footer"> 
-                                                            <div class="row justify-content-center">  
-                                                                <div class="col-md-3">
-                                                                    <a href="/cortePelo/{{ $cortePelo->imagen}}" download="/cortePelo/{{ $cortePelo->imagen}}"><span style="font-size: 2em; color: grey;"><i class="fas fa-download"></i></span></a>                       
-                                                                </div>
-                                                                <div class="col-md-3">
-                                                                    <a href=""><span style="font-size: 2em; color: grey;"><i class="fas fa-comment"></i></span></a>
-                                                                </div>   
-                                                            </div>                                  
-                                                        </div>
-                                                    </div> <?php
-                                                }
-                                            } 
-                                        }
-                                    }
-                                }else{
-                                    if($cortePelos->count()){
-                                        foreach($cortePelos as $cortePelo){ ?>
-                                            <div class="col-sm-4 ">
+                                @if($cortePelos->count())
+                                    @foreach($cortePelos as $cortePelo)
+                                        <div class="col-sm-4 ">
+                                            <div class="container-fluid ">  
                                                 <div class="img-container">
                                                     <div class="panel-body justify-content-center imagen">
                                                         <a class="thumbnail fancybox" rel="ligthbox" href="/cortePelo/{{ $cortePelo->imagen}}">
@@ -150,21 +102,26 @@
                                                             <p>{{$cortePelo->descripcion}}</p>
                                                         </a> 
                                                     </div>
-                                                </div>
+                                                 </div>
                                                 <div class="panel-footer"> 
                                                     <div class="row justify-content-center">  
-                                                        <div class="col-md-3">
-                                                            <a href="/cortePelo/{{ $cortePelo->imagen}}" download="/cortePelo/{{ $cortePelo->imagen}}"><span style="font-size: 2em; color: grey;"><i class="fas fa-download"></i></span></a>                       
+                                                        <div class="col-md-4">
+                                                            <a href="/cortePelo/{{ $cortePelo->imagen}}" download="/cortePelo/{{ $cortePelo->imagen}}"><span style="font-size: 2em; color: grey;">
+                                                                <i class="fas fa-download"></i>
+                                                            </span></a>                       
                                                         </div>
-                                                        <div class="col-md-3">
-                                                             <a href=""><span style="font-size: 2em; color: grey;"><i class="fas fa-comment"></i></span></a>
+                                                        <div class="col-md-4">
+                                                            <a href=""><span style="font-size: 2em; color: grey;">
+                                                                <i class="fas fa-comment"></i>
+                                                            </span></a>
                                                         </div>   
-                                                    </div>                                                                    
+                                                    </div>                                  
                                                 </div>
-                                            </div> <?php
-                                        }
-                                    } 
-                                }  ?>
+
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                @endif  
                             </div>
                         </div>
                     </div>
