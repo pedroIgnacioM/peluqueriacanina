7dd91865a6c880d0aec7781a9c48e6756c98e1f3
PedroIgnacioMM <pedro.ignaciom95@gmail.com>
2018-11-28 02:03:05 -0300

    feat: Ruta formulario simple

--- peluqueriaCanina/routes/web.php
+++ peluqueriaCanina/routes/web.php
@@ -24,23 +24,26 @@ Route::group(['middleware' => 'is_admin'], function () {
 
 });
 
+//Rutas Perfil
 Route::get('/formulario_mascota/agregar', 'MascotaController@formularioAgregar')->name('agregarMascota');
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/perfil/{nombre}','PerfilController@index')->name('perfil');
+
+Route::post('/agregarMascota','MascotaController@agregarMascota')->name('insertarMascota');
+Route::post('/subirImagenPerfil','PerfilController@subirImagen')->name('subirImagenPerfil');
+Route::post('/editarperfil','PerfilController@editarPerfil')->name('editarPerfil');
+
 
 //Rutas galeria
 Route::resource('galeria', 'CortePeloController');
 
 Route::get('/galeria', 'CortePeloController@index_default')->name('galeria');
-Route::post('/galeria/filtro', 'CortePeloController@galeriaFiltro')->name('galeriaFiltro');
 Route::get('cortePelo/', 'CortePeloController@download');
 
 
-//Rutas POST
-Route::post('/agregarMascota','MascotaController@agregarMascota')->name('insertarMascota');
-Route::post('/subirImagenPerfil','PerfilController@subirImagen')->name('subirImagenPerfil');
-Route::post('/editarperfil','PerfilController@editarPerfil')->name('editarPerfil');
+Route::post('/galeria/filtro', 'CortePeloController@galeriaFiltro')->name('galeriaFiltro');
+Route::post('/galeria/agregar','CortePeloController@store')->name('agregarCorte');
+
 
 
 
