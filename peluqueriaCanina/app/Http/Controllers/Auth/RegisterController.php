c04a6c4f929e48de1c1f3dc3defd1f51f5192c75
PedroIgnacioMM <pedro.ignaciom95@gmail.com>
2018-12-02 01:20:35 -0300

    feat: Variables del users para registrarse (Controlador)

--- peluqueriaCanina/app/Http/Controllers/Auth/RegisterController.php
+++ peluqueriaCanina/app/Http/Controllers/Auth/RegisterController.php
@@ -56,12 +56,9 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'nickname' => ['required', 'string', 'max:40', 'unique:users'],
-            'rut' => ['required', 'string', 'max:10'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'nombres' => ['required', 'string', 'max:255'],
+            'correo' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
-            'type' => User::DEFAULT_TYPE,
         ]);
     }
 
@@ -76,15 +73,10 @@ class RegisterController extends Controller
         
         $imagen = $data['imagen']->store('public/perfiles');
         $user=User::create([
-            'name' => $data['name'],
-            'nickname' => $data['nickname'],
-            'rut' => $data['rut'],
+            'nombres' => $data['nombres'],
+            'apellidos' => $data['apellidos'],
             'telefono' => $data['telefono'],
-            'ciudad' => $data['ciudad'],
-            'direccion' => $data['direccion'],
-            'edad' => $data['edad'],
-            'sexo' => $data['sexo'],
-            'email' => $data['email'],
+            'correo' => $data['correo'],
             'password' => Hash::make($data['password']),
             'imagen'=>$imagen,
         ]);
