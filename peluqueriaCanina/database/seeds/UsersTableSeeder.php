d10cfff8950bf2c5c5f24b71bcf71211972f84a5
PedroIgnacioMM <pedro.ignaciom95@gmail.com>
2018-12-02 01:06:56 -0300

    feat: Se modifican las variables en la tabla user de la BD

--- peluqueriaCanina/database/seeds/UsersTableSeeder.php
+++ peluqueriaCanina/database/seeds/UsersTableSeeder.php
@@ -14,30 +14,20 @@ class UsersTableSeeder extends Seeder
         $sexo=['masculino','femenino','otros'];
         foreach (range(1,10) as $elemento) {
         DB::table('users')->insert([
-            'name' => $faker->firstName(),
-            'email' => $faker->email,
+            'nombres' => $faker->firstName(),
+            'apellidos' =>$faker->lastName,
+            'correo' => $faker->email,
             'password'=>bcrypt('abc123456'),
-            'nickname'=>$faker->userName,
-            'rut'=>$faker->unique()->randomNumber($nb=9),
             'telefono'=>$faker->unique()->randomNumber($nb=9),
-            'ciudad'=>$faker->city,
-            'direccion'=>$faker->address,
-            'edad'=>$faker->randomNumber($nb=2),
-            'sexo'=>$faker->randomElement($sexo),
         ]);}
 
         DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('abc123456'),
-            'type' =>'admin',
-            'nickname'=>'administrador',
-            'rut'=>'12345678-9',
+            'nombres' => 'admin',
+            'apellidos' =>'admin admin',
             'telefono'=> 123854383,
-            'ciudad'=> "Valparaiso",
-            'direccion'=>'calle prat 1234',
-            'edad'=>80,
-            'sexo'=>"masculino"
+            'correo' => 'admin@gmail.com',
+            'password' => bcrypt('abc123456'),
+            'tipo' =>'admin',
 
         ]);
     }
