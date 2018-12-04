ca8de990f20d28a037393e42a6a59396d86116d1
Javier <javier.maul@gmail.com>
2018-12-04 01:38:28 -0300

    cambio de tamaño en galeria, catalogo, home.
    asd

--- peluqueriaCanina/resources/views/catalogo.blade.php
+++ peluqueriaCanina/resources/views/catalogo.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 <div class="container-fluid">
-        <div class="col-sm-12">
+    <div class="row justify-content-center">
+        <div class="col-sm-10">
             <div class="card">
                 <div class="card-body">
                     <div class="row">
@@ -82,6 +83,7 @@
             </div>
         </div>
 
+        </div>
     </div>
 </div>
 @endsection
--- peluqueriaCanina/resources/views/home.blade.php
+++ peluqueriaCanina/resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="justify-content-center">
-        
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
             <div class="card">
                 
                 <div class="card-body">
@@ -117,7 +117,7 @@
                 </div>
 
             </div>
-        
+        </div>
     </div>
 </div>
 @endsection
