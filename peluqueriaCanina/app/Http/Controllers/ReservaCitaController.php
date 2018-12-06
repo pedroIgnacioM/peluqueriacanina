b4a1872ded40f0ec53256fb9041e48650bc81e34
PedroIgnacioMM <pedro.ignaciom95@gmail.com>
2018-12-06 02:35:21 -0300

    feat: Se agrega la funcion para obtener las horas disponibles

--- peluqueriaCanina/app/Http/Controllers/ReservaCitaController.php
+++ peluqueriaCanina/app/Http/Controllers/ReservaCitaController.php
@@ -23,23 +23,50 @@ class ReservaCitaController extends Controller
             $diasSemana[$numero]=$diaLunes;
             $diaLunes++;    
         }
-        // $horariosDisponibles = $this->horariosDisponibles($dias,$mes);
+        //Se obtiene las horas disponibles
+        $mesNumero = strftime('%m');
+        $horariosDisponibles = $this->horariosDisponibles($diasSemana,$mesNumero);
 
         return view('reservaCita',[
             'dias'=>$diasSemana,
             'mes'=>$mes,
             'dia'=>$diaActual,
+            'horariosLibres'=>$horariosDisponibles
         ]);
     }
 
-    // private function horariosDisponibles($dias,$mes)
-    // {
-    //     $i=0;
-    //     foreach ($dias as $dia) {
-    //         $reservas[$i] = ReservaCita::where('')
-    //                                     ->get();
+    private function horariosDisponibles($dias,$mes)
+    {
+        $i=0;
+        $lunesSabado=['9:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00'];
+        $domingo=['9:00','10:00','11:00','12:00','13:00'];
+        
+        //Se recorren los dias de la semana recibida
+        foreach ($dias as $dia) {
+            $reservas[$i] = ReservaCita::whereMonth('fecha',$mes)
+                                        ->whereDay('fecha',$dia)
+                                        ->get();
 
-    //         $i++;
-    //     }
-    // }
+            $j=0;
+            $horasOcupadas=null;
+            //Se obtiene un array solo con las horas ocupadas                            
+            foreach ($reservas[$i] as $reserva) {
+                $horasOcupadas[$j]=date("H:i", strtotime($reserva->fecha));
+                $j++;
+            }
+            //Se obtiene un array con las horas reservables dependiendo el dia
+            if($i=='6')
+                $horarios[$i]=$domingo;
+            else
+                $horarios[$i]=$lunesSabado;
+            
+            //Elimina las horas reservadas de las horas reservables
+            if($horasOcupadas)
+                $horariosDisponibles[$i]=array_diff($horarios[$i],$horasOcupadas);
+            else
+                $horariosDisponibles[$i]=$horarios[$i];
+            $i++;
+        }
+        return $horariosDisponibles;
+    }
 }
