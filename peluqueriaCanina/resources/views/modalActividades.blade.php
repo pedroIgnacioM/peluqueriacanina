824a24aa1c0c6c0dacb74f048600a7a3fe61b479
PedroIgnacioMM <pedro.ignaciom95@gmail.com>
2018-12-07 23:12:35 -0300

    feat: Modal actividades

---
+++ peluqueriaCanina/resources/views/modalActividades.blade.php
@@ -0,0 +1,34 @@
+<div class="modal-dialog" role="document">
+    <div class="modal-content">
+        <div class="modal-header">
+            <h2 class="modal-title" id="myModalLabel">Actividad</h2>
+            <button type="button" class="close" data-dismiss="modal">
+                <span> <i class="fas fa-times"></i></span>
+            </button> 
+        </div>
+        <div class="modal-body">
+            @if ($actividades->isEmpty())
+                <p>No existen actividades en este momento</p>
+            @else
+            <table class="table table-bordered">
+                <tr>
+                    <th>Nombre</th>
+                    <th>Usuario</th>
+                    <th>Fecha</th>
+                </tr>
+                @foreach ($actividades as $actividad)
+                    <tr>
+                        <td>{{$actividad->user->nombres}}</td>
+                        <td>{{$actividad->user->email}}</td>
+                        <td>{{$actividad->created_at}}</td>
+                    </tr>
+                @endforeach
+            </table>
+            @endif
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+            </div>
+        </div>
+        
+    </div>
+</div>
