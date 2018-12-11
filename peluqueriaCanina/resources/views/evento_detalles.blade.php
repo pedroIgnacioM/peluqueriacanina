ea65ee95d379b9b915f457b03a4b3c4ba4517af1
PedroIgnacioMM <pedro.ignaciom95@gmail.com>
2018-12-11 03:14:51 -0300

    fix: tamaño del contenido cambiante

--- peluqueriaCanina/resources/views/evento_detalles.blade.php
+++ peluqueriaCanina/resources/views/evento_detalles.blade.php
@@ -5,41 +5,39 @@
 <div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-sm-10">
-            <div class="row justify-content-center">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row justify-content-between">
-                            <div class="col-md-6">
-                                <div class="card-title text-capitalize"><h1>{{$evento->titulo}}</h1></div>
-                                
-                                <div class="row">
-                                    <div class="col-md-7">
-                                        <p id="txtDate">{{$evento->fecha}}</p>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-md-8">
-                                        <p>{{$evento->direccion}}</p>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-md-10">
-                                        <p>{{$evento->descripcion}}</p>
-                                    </div>
-                                </div>
-
-                                <div class="row">
-                                    <div class="col-md-7">
-                                        <p>{{$evento->estado}}</p>
-                                    </div>
+            <div class="card">
+                <div class="card-body">
+                    <div class="row justify-content-between">
+                        <div class="col-md-6">
+                            <div class="card-title text-capitalize"><h1>{{$evento->titulo}}</h1></div>
+                            
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <p id="txtDate">{{$evento->fecha}}</p>
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="container-fluid">
-                                    <img class="imagenEvento" src="{{Storage::url($evento->imagen)}}" alt="">
+
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <p>{{$evento->direccion}}</p>
                                 </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-10">
+                                    <p>{{$evento->descripcion}}</p>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-7">
+                                    <p>{{$evento->estado}}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="container-fluid">
+                                <img class="imagenEvento" src="{{Storage::url($evento->imagen)}}" alt="">
                             </div>
                         </div>
                     </div>
