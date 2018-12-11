de6830a32e3bacbebd11a807391eacf853c7d973
PedroIgnacioMM <pedro.ignaciom95@gmail.com>
2018-12-11 04:04:41 -0300

    feat: Se agrega la ruta de eventos al nav

--- peluqueriaCanina/resources/views/layouts/app.blade.php
+++ peluqueriaCanina/resources/views/layouts/app.blade.php
@@ -86,6 +86,9 @@
                             <a class="dropdown-item" href="{{route('perfil',['Usuario'])}}">
                                 Perfil
                             </a>
+                            <a class="dropdown-item" href="{{route('eventos')}}">
+                                    Eventos
+                                </a>
                              <a class="dropdown-item" href="{{ route('logout') }}"
                                                 onclick="event.preventDefault();
                                                                 document.getElementById('logout-form').submit();">
