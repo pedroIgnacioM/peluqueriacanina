ab87b6f1e5f5dcdd7e33541100a29187e24b6c81
PedroIgnacioMM <pedro.ignaciom95@gmail.com>
2018-12-13 17:04:06 -0300

    feat: Se agrega atributos fillables

--- peluqueriaCanina/app/Producto.php
+++ peluqueriaCanina/app/Producto.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
+    protected $fillable = [
+        'nombre','descripcion','precio','imagen',
+    ];
     public function ReservaProducto(){
         return $this->belongsTo('App\ReservaProducto');
     }
