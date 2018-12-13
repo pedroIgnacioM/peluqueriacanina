2ee3b24956aa4f733ce41d15394e3e36bfaf5294
PedroIgnacioMM <pedro.ignaciom95@gmail.com>
2018-12-13 17:04:21 -0300

    feat: Se agrega controlador para agregar producto

--- peluqueriaCanina/app/Http/Controllers/ProductosController.php
+++ peluqueriaCanina/app/Http/Controllers/ProductosController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\producto;
+use App\Producto;
 
 class ProductosController extends Controller
 {
@@ -14,75 +14,25 @@ class ProductosController extends Controller
      */
     public function index()
     {
-        $productos = producto::orderBy('id','DESC')->paginate(9);
+        $productos = Producto::orderBy('id','DESC')->paginate(9);
         return view('catalogo')->with('productos',$productos);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *|
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    
+    public function agregar(Request $request){
+
+        $imagen = $request->file('imagen')->store('public/productos'); 
+            Producto::create([
+            'nombre' => $request->nombre,
+            'descripcion' => $request->descripcion,
+            'precio' => $request->precio,
+            'imagen'=>$imagen,
+
+        ]);
+    
+        return redirect()->route('catalogo')->with('success','Registro creado satisfactoriamente');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
     public function catalogoFiltro(Request $request){
 
  
