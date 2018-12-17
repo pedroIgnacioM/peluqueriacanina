5ff3dc49203a31bdf176e447876102036bb47c0a
PedroIgnacioMM <pedro.ignaciom95@gmail.com>
2018-12-17 03:34:34 -0300

    feat: Se llama al modal agregar corte pelo por metodo get

--- peluqueriaCanina/resources/views/galeria.blade.php
+++ peluqueriaCanina/resources/views/galeria.blade.php
@@ -14,7 +14,9 @@
                                         @auth
                                             @if(Auth::user()->isAdmin())
                                                 {{-- Botón Agregar --}}
-                                                <button class="btn btn-lg btn-primary" data-toggle="modal" data-target="#agregarCortePelo">Agregar Imagen <i class="fas fa-plus"></i></button>
+                                                <a class="botonModalAgregarCorte" href="#" data-toggle="modal" data-form="{{route('agregarCorteModal')}}" data-target="#modal-agregar-corte">
+                                                    <button class="btn btn-lg btn-primary">Agregar Imagen <i class="fas fa-plus"></i></button>
+                                                </a>
                                             @endif
                                         @endauth
                                     </div>
@@ -187,95 +189,6 @@
     </div>
 </div>
 
-{{-- Modal del boton (+) --}}
-<div class="modal fade" id="agregarCortePelo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-    <div class="modal-dialog modal-dialog2" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h4 class="modal-title" id="myModalLabel">Agregar Corte de pelo</h4>
-                <button type="button" class="close" data-dismiss="modal">
-                    <span> <i class="fas fa-times"></i></span>
-                </button> 
-            </div>
-            <form action="{{route('agregarCorte')}}" method="post" enctype="multipart/form-data">
-                {{csrf_field()}}
-                <div class="modal-body">
-                    <div class="form-group row justify-content-md-center">
-                        <div class="col-md-12">
-                            {{-- Tipo --}}
-                            <div class="form-group row">
-                                <div class="col-md-3">
-                                    <label for="tipo" class="col-form-label text-md-right">{{ __('Tipo') }}</label>
-                                </div>
-                                <div class="col-md-5">
-                                    <select id="tipo" class="custom-select form-control tipo ? ' is-invalid' : '' }}" name="tipo" required autofocus>
-                                        <option value="" selected disabled>Seleccionar</option>
-                                        <optgroup label="Tipos de servicios">
-                                        <option value="solo corte">Solo corte</option>
-                                        <option value="baño y corte">Baño + Corte</option>
-                                        <option value="solo baño">Solo baño</option>
-                                    </select>
-                                </div>
-                            </div>
-                            {{-- Tamaño --}}
-                            <div class="form-group row">
-                                <div class="col-md-3">
-                                    <label for="tamano" class="col-form-label text-md-right">{{ __('Tamaño') }}</label>
-                                </div>
-                                <div class="col-md-5">
-                                    <select id="tamano" class="custom-select form-control tipo ? ' is-invalid' : '' }}" name="tamano" required autofocus>
-                                        <option value="" selected disabled>Seleccionar</option>
-                                        <optgroup label="Tamaños de Perros">
-                                        <option value="pequeño">Pequeño</option>
-                                        <option value="mediano">Mediano</option>
-                                        <option value="grande">Grande</option>
-                                    </select>
-                                </div>
-                            </div>
-                            {{-- Tipo Cabello --}}
-                            <div class="form-group row">
-                                    <div class="col-md-3">
-                                        <label for="cabello" class="col-form-label text-md-right">{{ __('Cabello') }}</label>
-                                    </div>
-                                    <div class="col-md-5">
-                                        <select id="cabello" class="custom-select form-control tipo ? ' is-invalid' : '' }}" name="cabello" required autofocus>>
-                                            <option value="" selected disabled>Seleccionar</option>
-                                            <optgroup label="Tipos de Cabellos">
-                                            <option value="1">Rubio</option>
-                                            <option value="2">Castaño</option>
-                                            <option value="3">Pelo liso</option>
-                                        </select>
-                                    </div>
-                            </div>
-                            {{-- Descripción --}}
-                            <div class="form-group row">
-                                <div class="col-md-3">
-                                    <label for="descripcion" class="col-form-label text-md-right">{{ __('Descripción') }}</label>
-                                </div>
-                                <div class="col-md-8">
-                                    <textarea id="descripcion" rows="4" class="form-control tipo ? ' is-invalid' : '' }}" name="descripcion" required autofocus></textarea>
-                                </div>
-                            </div>
-                            {{-- Imagen --}}
-                            <div class="form-group row">
-                                <div class="col-md-3">
-                                    <label for="imagen" class="col-form-label text-md-right">{{ __('Imagen') }}</label>
-                                </div>
-                                <div class="col-md-9">
-                                    <input id="imagen" type="file" name="imagen" required>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                        <button type="submit" class="btn btn-primary">Guardar</button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
 <div class="modal" id="modalImagen">
     <div class="ventana">
         
@@ -285,6 +198,7 @@
         </div>
     </div>
 </div>
+<div class="modal" id="modal-agregar-corte"></div>
 <div class="modal" id="modal-corte"></div>
 <div class="modal" id="modal-corteFavorito"></div>
 <div class="modal" id="modal-comentario"></div>
@@ -326,6 +240,28 @@
             });
         });
 
+        // Modal Agregar Corte de Pelo
+        $(".botonModalAgregarCorte").click(function (ev) { // for each edit contact url
+            ev.preventDefault(); // prevent navigation
+            var url = $(this).data("form"); // get the contact form url
+            console.log(url);
+            $("#modal-agregar-corte").load(url, function () { // load the url into the modal
+                $(this).modal('show'); // display the modal on url load
+            });
+        });
+
+        $('.agregar-corte-form').on('submit', function () {
+            $.ajax({
+                type: $(this).attr('method'),
+                url: $(this).attr('action'),
+                data: $(this).serialize(),
+                context: this,
+                success: function (data, status) {
+                    $('#modal-agregar-corte').html(data);
+                }
+            });
+        });
+        
         // Modal Favorito
         $(".botonModalFavorito").click(function (ev) { // for each edit contact url
             ev.preventDefault(); // prevent navigation
